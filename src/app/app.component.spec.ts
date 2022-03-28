c68f46abaf934cf46acf7e5b2525a67a66e9eac3
Frankie Miklos <fmiklos@modusoperandi.com>
2022-03-28 10:07:12 -0400

    Fix unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('interview-project');
   });
 
-  it('should render title', () => {
+  it('should render header', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('interview-project app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toEqual('Your work goes here!');
   });
 });
